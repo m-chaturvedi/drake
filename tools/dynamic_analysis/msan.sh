6e9eafcc6cb8171a50f7101cc65aaf49451f8d3e
Mmanu Chaturvedi <mmanu.chaturvedi@kitware.com>
2017-11-09 15:09:51 -0500

    MSan Configuration

--- tools/dynamic_analysis/msan.sh
+++ tools/dynamic_analysis/msan.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 me=$(python -c 'import os; print(os.path.realpath("'"$0"'"))')
 mydir=$(dirname "$me")
+export LD_LIBRARY_PATH=/home/chaturvedi/workspace/libcxx_msan/lib
 export MSAN_OPTIONS="$MSAN_OPTIONS:suppressions=$mydir/msan.supp"
 "$@"
